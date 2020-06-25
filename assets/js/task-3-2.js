14ce96fe3ea17fb7c38ddab48edc8a52d46b2573
Johnny <Johnny.savu.99@gmail.com>
2020-06-25 16:31:43 +0300

    .

--- assets/js/task-3-2.js
+++ assets/js/task-3-2.js
@@ -10,7 +10,7 @@ var quiz = [
     },
     {
         intrebare : "Who is the main character of 'The book thief'?",
-        respunsuri : [["Markus Zusak", false], ["Joseph K", false], ["Liesel Meminger", true], ["Elizabeth Bennet", false]] 
+        raspunsuri : [["Markus Zusak", false], ["Joseph K", false], ["Liesel Meminger", true], ["Elizabeth Bennet", false]] 
     },
     {
         intrebare : "What is the origin of the author Franz Kafka?",
@@ -22,3 +22,78 @@ var quiz = [
     }
 ]
 
+function display_quiz()
+{
+    let main = document.getElementById('main-section');
+    main.innerHTML = `<main class = 'main-div'> 
+        <ol id = "main-quiz-ol" style = "text-align:left">
+ 
+            </ol>
+    </main>`;
+    main = document.getElementById('main-quiz-ol');
+    for (let i = 0; i < quiz.length; i++)
+    {
+
+        main.innerHTML += "<li>" + quiz[i].intrebare + "<ol type = 'a'> </ol> </li>";
+        let ans = main.lastChild;
+        ans = ans.getElementsByTagName("ol")[0];
+        for (let j = 0; j < quiz[i].raspunsuri.length; j++)
+        {
+            ans.innerHTML +=`<li><input type='checkbox' name = '${j}'> <label for = '${j}'>` + quiz[i].raspunsuri[j][0] + `</label></li>`
+        }
+
+    }
+    main = main.parentElement;
+    main.innerHTML += '<button type = "button" class = "submit-button" id = "get-result-btn">Show result</button>';
+    document.getElementById("get-result-btn").addEventListener("click", function(){
+        getResult();
+    });
+
+}
+
+
+function getResult()
+{
+    let score = 0;
+    let count_quiz = 0;
+    let count_ans = 0;
+    let intrebari = document.getElementById("main-quiz-ol").children;
+    //console.log(intrebari);
+    //console.log(intrebari[0]);
+    for (let i = 0; i < intrebari.length; i++)
+    {
+        let intrebare = intrebari[i];
+        //console.log(intrebare.innerHTML);
+        let raspunsuri = intrebare.getElementsByTagName("ol")[0].children;
+        let ok = 1;
+        for (let j = 0; j < raspunsuri.length; j++)
+        {
+            let raspuns = raspunsuri[j];
+            //console.log(raspuns.getElementsByTagName("input")[0].checked);
+            if (quiz[i].raspunsuri[j][1] && !raspuns.getElementsByTagName("input")[0].checked)
+            {
+                ok = 0;
+            }
+            if (!quiz[i].raspunsuri[j][1] && raspuns.getElementsByTagName("input")[0].checked)
+            {
+                ok = 0;
+            }
+            raspuns.getElementsByTagName("input")[0].disabled = true;
+
+        }
+        if (ok == 1)
+            score += 1;
+    }
+    let main = document.getElementsByClassName("main-div")[0];
+    main.innerHTML += `<p style = 'font-size:40px'">Your score is: ${score} </p>`;
+}
+
+
+document.addEventListener("click", function(e)
+{
+    if (e.target && e.target.id == 'go-quiz-btn')
+    {
+        display_quiz();
+    }
+
+});
